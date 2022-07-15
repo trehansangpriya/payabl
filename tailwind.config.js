8b296347c8a35b40acd576af817d5ec0cd3ef999
Trehan Sangpriya <trehansangpriya@gmail.com>
2022-07-15 11:30:40 +0530

    cleaned index page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
