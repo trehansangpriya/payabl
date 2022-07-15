d9cd944087ef5687f0387e93aa2a23a44a7a7169
Trehan Sangpriya <trehansangpriya@gmail.com>
2022-07-15 12:32:28 +0530

    uninstalled mui

--- pages/_document.js
+++ pages/_document.js
@@ -11,11 +11,6 @@ class MyDocument extends Document {
             <Html>
                 <Head>
                     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
-                    <link
-                        rel="stylesheet"
-                        href="https://fonts.googleapis.com/icon?family=Material+Icons"
-                    />
-
                 </Head>
                 <body>
                     <Main />
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Button } from '@mui/material'
 const Home = () => {
 
   return (
