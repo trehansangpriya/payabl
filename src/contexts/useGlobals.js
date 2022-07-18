992ed8238954f2c875feb97bd8326689700d7692
Trehan Sangpriya <trehansangpriya@gmail.com>
2022-07-18 19:22:29 +0530

    Added Global App Context

--- src/contexts/useGlobals.js
+++ src/contexts/useGlobals.js
@@ -2,12 +2,17 @@ import React, { createContext, useContext, useMemo, useState } from "react";
 
 export const GlobalContext = createContext({})
 export const GlobalProvider = ({ children }) => {
+    // App Loading
     const [loading, setLoading] = useState(false)
+
+    // App Alerts
     const [showAlert, setShowAlert] = useState({
         show: false,
         message: '',
         variant: '',
     })
+
+    // Function to Toggle Alert
     const displayAlert = (show, variant, message) => {
         setShowAlert({
             show,
