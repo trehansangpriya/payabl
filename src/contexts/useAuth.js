1e037eda8d05df8004fe31669740c2f50f3b8314
Trehan Sangpriya <trehansangpriya@gmail.com>
2022-07-18 19:31:41 +0530

    basic auth functionality added

---
+++ src/contexts/useAuth.js
@@ -0,0 +1,84 @@
+import { GoogleAuthProvider, onAuthStateChanged, signInWithRedirect, signOut } from "firebase/auth";
+import React, { createContext, useEffect, useState, useMemo, useContext } from "react";
+import { auth, db } from "@/Firebase/index";
+import useGlobals from "@/Contexts/useGlobals"
+import { doc, onSnapshot, serverTimestamp, setDoc } from "firebase/firestore";
+
+export const AuthContext = createContext({})
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [authLoading, setAuthLoading] = useState(true)
+    const [dataLoading, setDataLoading] = useState(false)
+    const [userData, setUserData] = useState(null)
+
+    // Global Context states
+    const { setLoading } = useGlobals()
+
+    useEffect(() => onAuthStateChanged(auth, (user) => {
+        if (user) {
+            setUser(user)
+            setDataLoading(true)
+            onSnapshot(doc(db, 'users', user.uid), (snapshot) => {
+                if (!snapshot.exists()) {
+                    setDoc(doc(db, 'users', user.uid), {
+                        displayName: user.displayName,
+                        email: user.email,
+                        photoURL: user.photoURL,
+                        createdAt: serverTimestamp(),
+                        profileComplete: false,
+                    })
+                    setDataLoading(false)
+                } else {
+                    setUserData(snapshot.data())
+                    setDataLoading(false)
+                }
+            })
+
+        }
+        else {
+            setUser(null)
+        }
+        // setTimeout(() => setAuthLoading(false), 1000)
+        setAuthLoading(false)
+    }), [])
+    const provider = new GoogleAuthProvider()
+    const signInWithGoogle = () => (
+        signInWithRedirect(auth, provider)
+    )
+    const logOut = () => {
+        setLoading(true)
+        signOut(auth).catch(error => {
+            setError(error)
+        }).finally(() => {
+            setLoading(false)
+        })
+    }
+
+    console.log(user)
+    // value using useMemo
+    const value = {
+        user,
+        setUser,
+        authLoading,
+        setAuthLoading,
+        userData,
+        setUserData,
+        dataLoading,
+        setDataLoading,
+        signInWithGoogle,
+        logOut,
+    }
+    return (
+        <AuthContext.Provider value={value}>
+            {
+                !authLoading ? children : <div>Loading...</div>
+            }
+        </AuthContext.Provider>
+    )
+}
+
+const useAuth = () => {
+    return useContext(AuthContext)
+}
+
+export default useAuth
