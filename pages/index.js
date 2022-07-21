b589d46f2ea5106b014cc8004fc3c0b3227fb118
Trehan Sangpriya <trehansangpriya@gmail.com>
2022-07-21 13:19:18 +0530

    Updated Home page Wrapper from div to Container

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Alert, Button, Container, Loading } from '@/Components/utility'
+import { Alert, Button, Card, Container, Loading } from '@/Components/utility'
 import { privateRoute } from '@/Routes/privateRoute'
 import useGlobals from '@/Contexts/useGlobals';
 import { FiLogOut } from 'react-icons/fi'
@@ -8,26 +8,24 @@ const Home = ({ auth }) => {
   const { user, userData, logOut } = auth
   const { displayAlert } = useGlobals()
   return (
-    <div>
-      <Container
-        gap={'16px'}
-        padding='12px'
-        minHeight='100vh'
-      >
-        <h1>
-          Home
-        </h1>
-        {user && userData && `Welcome, ${userData?.displayName}`}
-        {user &&
-          <Button
-            color='error'
-            iconLeft={<FiLogOut />}
-            onClick={logOut}
-          >
-            Log Out
-          </Button>}
-      </Container>
-    </div>
+    <Container
+      gap={'16px'}
+      padding='12px'
+      minHeight='100vh'
+    >
+      <h1>
+        Home
+      </h1>
+      {user && userData && `Welcome, ${userData?.displayName}`}
+      {user &&
+        <Button
+          color='error'
+          iconLeft={<FiLogOut />}
+          onClick={logOut}
+        >
+          Log Out
+        </Button>}
+    </Container>
   )
 }
 
