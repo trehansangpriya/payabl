3f3e3f46333c802ca1c88813a400881b3ae9b53a
Trehan Sangpriya <trehansangpriya@gmail.com>
2022-07-22 11:20:18 +0530

    Updated Button : set initial state to the color prop

--- src/components/utility/Button/index.js
+++ src/components/utility/Button/index.js
@@ -16,19 +16,21 @@ const Button = ({
     // Global loading state
     const { loading } = useGlobals()
 
+    // Button Colors
+    const buttonColors = {
+        primary: 'text-layout-100 bg-primary-500 hover:bg-primary-600 active:bg-primary-600',
+        error: 'text-layout-100 bg-error-500 hover:bg-error-600 active:bg-error-600',
+        success: 'text-layout-100 bg-success-500 hover:bg-success-600 active:bg-success-600',
+        warn: 'text-layout-100 bg-warn-500 hover:bg-warn-600 active:bg-warn-600',
+        info: 'text-layout-100 bg-info-500 hover:bg-info-600 active:bg-info-600',
+    }
+
     // Button Color State
-    const [buttonColor, setButtonColor] = useState('')
+    const [buttonColor, setButtonColor] = useState(buttonColors[color])
 
     useEffect(() => {
-        // Button Colors
-        const buttonColors = {
-            primary: 'text-layout-100 bg-primary-500 hover:bg-primary-600 active:bg-primary-600',
-            error: 'text-layout-100 bg-error-500 hover:bg-error-600 active:bg-error-600',
-            success: 'text-layout-100 bg-success-500 hover:bg-success-600 active:bg-success-600',
-            warn: 'text-layout-100 bg-warn-500 hover:bg-warn-600 active:bg-warn-600',
-            info: 'text-layout-100 bg-info-500 hover:bg-info-600 active:bg-info-600',
-        }
         setButtonColor(buttonColors[color])
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [color])
     return (
         <button
