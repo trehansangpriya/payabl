bfd5116ab95369dc69afbbce7a3216b84afcf98d
Trehan Sangpriya <trehansangpriya@gmail.com>
2022-07-25 11:52:22 +0530

    Login Screen UI 1st Iteration : Mobile Only

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
-import { Button, Container, Loading } from '@/Components/utility'
+import { Button, Container, Loading, Text } from '@/Components/utility'
 import { publicRoute } from '@/Routes/publicRoute'
 import { FaGoogle } from 'react-icons/fa';
 import useGlobals from '@/Contexts/useGlobals';
+import Image from 'next/image';
 
 const Login = ({ auth }) => {
     // Sign in with Google from Firebase
@@ -30,15 +31,51 @@ const Login = ({ auth }) => {
 
     return (
         <Container
-            minHeight='100vh'
+            id='login-screen'
+            className={[
+                'h-screen flex flex-col justify-start',
+                'lg:flex-row'
+            ].join(' ')}
         >
-            <Button
-                iconLeft={<FaGoogle />}
-                onClick={handleSignIn}
-                disabled={loading}
-            >Sign In With Google</Button>
+            {/* Lg: Left Sm: Top */}
+            <Container
+                id="logo-wrapper"
+                className={[
+                    'w-full h-full',
+                    'flex justify-start items-center',
+                ].join(' ')}
+            >
+                <Image
+                    src='/assets/images/logo.png'
+                    alt='Payabl'
+                    width={360}
+                    height={360}
+                />
+            </Container>
+            {/* Lg: Right Sm: Bottom */}
+            <Container
+                id="login-wrapper"
+                className={[
+                    'w-full h-full p-4',
+                    'flex flex-col gap-6 justify-end'
+                ].join(' ')}
+            >
+                <Text
+                    tag='h1'
+                    className='text-6xl'
+                >
+                    Payabl
+                </Text>
+                <Button
+                    className='w-full'
+                    iconLeft={<FaGoogle />}
+                    onClick={handleSignIn}
+                    disabled={loading}
+                >Sign In With Google</Button>
+            </Container>
+            {/* Sign In Loading */}
             {
-                loading && <Loading inline message='Waiting for you to sign in with Google...' />
+                loading && <Loading modal message='Waiting for you to sign in with Google...' />
             }
         </Container>
     )
