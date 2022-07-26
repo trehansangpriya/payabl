ccf2cfd4fe9d008d6d7de3a271dc6a7288f2edbf
Trehan Sangpriya <trehansangpriya@gmail.com>
2022-07-26 12:47:26 +0530

    Pages: replaces general text with Text Component. Closes App Components #24

--- pages/accounts.js
+++ pages/accounts.js
@@ -1,10 +1,15 @@
 import { AppScreen } from '@/Components/app'
+import { Text } from '@/Components/utility'
 import { privateRoute } from '@/Routes/privateRoute'
 import React from 'react'
 
 const Accounts = () => {
     return (
-        <AppScreen>Accounts</AppScreen>
+        <AppScreen>
+            <Text tag='h4'>
+                Accounts
+            </Text>
+        </AppScreen>
     )
 }
 
--- pages/transactions.js
+++ pages/transactions.js
@@ -1,10 +1,15 @@
 import { AppScreen } from '@/Components/app'
+import { Text } from '@/Components/utility'
 import { privateRoute } from '@/Routes/privateRoute'
 import React from 'react'
 
 const Transactions = () => {
     return (
-        <AppScreen>Transactions</AppScreen>
+        <AppScreen>
+            <Text tag='h4'>
+                Transactions
+            </Text>
+        </AppScreen>
     )
 }
 
