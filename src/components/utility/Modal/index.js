5fb05633bdc557ce543d89618ae8527001021c96
Trehan Sangpriya <trehansangpriya@gmail.com>
2022-07-27 15:21:15 +0530

    Updated Modal: structure and style

--- src/components/utility/Modal/index.js
+++ src/components/utility/Modal/index.js
@@ -14,25 +14,17 @@ const Modal = ({
     }
     return (
         <div className='w-full h-screen fixed top-0 right-0 bg-black bg-opacity-40 flex  items-center justify-center z-50'>
-            <div className="bg-white p-4 rounded shadow min-w-[320px] max-w-[80%] lg:max-w-[40%] flex flex-col gap-2 items-center justify-center">
+            <div className="bg-white p-4 rounded shadow min-w-[320px] max-w-[90%] lg:max-w-[40%] flex flex-col gap-4 items-center justify-center w-fit">
                 {title && (
                     <>
-                        <div className='w-full flex justify-start items-center'>
+                        <div className='w-full flex justify-between items-center gap-2'>
                             <h1 className='text-lg font-medium'>{title}</h1>
+                            <FiXCircle className='text-2xl cursor-pointer text-error-500' onClick={onClose} />
                         </div>
-                        <Seperator />
                     </>
                 )}
-                <div className={`w-full flex justify-center items-center ${className}`}>
+                <div className={`w-full flex ${className}`}>
                     {children}
-                </div>
-                <Seperator />
-                <div className='w-full flex justify-start items-center'>
-                    <Button
-                        color='error'
-                        iconLeft={<FiXCircle />}
-                        onClick={onClose}
-                    >Close</Button>
                 </div>
             </div>
         </div>
