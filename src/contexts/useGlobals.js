6dc8debd3ccaff4c02f6e9a3fe67f0bbfb766474
Trehan Sangpriya <trehansangpriya@gmail.com>
2022-07-27 12:01:43 +0530

    Updated useGlobals: removed unnecessary imports

--- src/contexts/useGlobals.js
+++ src/contexts/useGlobals.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useMemo, useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 
 export const GlobalContext = createContext({})
 export const GlobalProvider = ({ children }) => {
