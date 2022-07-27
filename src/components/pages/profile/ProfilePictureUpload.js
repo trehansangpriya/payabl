cd7d1e025f07dbdefb01c73a032f54d5bcfb60e0
Trehan Sangpriya <trehansangpriya@gmail.com>
2022-07-27 19:50:03 +0530

    Added Component: pages/profile/ProfilePictureUpload

---
+++ src/components/pages/profile/ProfilePictureUpload.js
@@ -0,0 +1,97 @@
+import React, { useState } from 'react'
+import { Button, Form, Input, Loading, Progress } from '@/Components/utility'
+import useAuth from '@/Contexts/useAuth'
+import useGlobals from '@/Contexts/useGlobals'
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage'
+import { storage } from '@/Firebase/index'
+import { FiUploadCloud } from 'react-icons/fi'
+import { doc, serverTimestamp, setDoc } from 'firebase/firestore'
+import { db } from '@/Firebase/index'
+
+
+const ProfilePictureUpload = ({
+    afterSubmitActions = () => { },
+}) => {
+    // Auth Context 
+    const { user } = useAuth()
+
+    // Global Context
+    const { displayAlert } = useGlobals()
+
+    // Local Loading State
+    const [loading, setLoading] = useState(false)
+
+    // Local Progress State
+    const [progress, setProgress] = useState(0)
+
+    // Image Upload State
+    const [image, setImage] = useState(null)
+
+    // Image Upload Handler
+    const handleImageUpload = (e) => {
+        setLoading(true)
+        const fileName = user.displayName.split(' ')[0].toLowerCase()
+        const profilePictureRef = ref(storage, `users/${fileName}${image.type.split('/')[1]}`)
+        const uploadProfilePicture = uploadBytesResumable(profilePictureRef, image)
+        uploadProfilePicture.on(
+            'state_changed',
+            (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                setProgress((snapshot.bytesTransferred / snapshot.totalBytes) * 100)
+            },
+            (err) => {
+                displayAlert(true, 'error', err.message)
+            },
+            () => {
+                displayAlert(true, 'success', 'Uploaded')
+                getDownloadURL(uploadProfilePicture.snapshot.ref)
+                    .then((url) => {
+                        setDoc(doc(db, 'users', user.uid), {
+                            photoURL: url,
+                            editedAt: serverTimestamp(),
+                        }, { merge: true })
+                            .then(() => {
+                                displayAlert(true, 'success', 'Profile Picture Updated')
+                                setLoading(false)
+                                setImage(null)
+                                afterSubmitActions()
+                            })
+                    })
+            }
+        )
+    }
+    return (
+        <Form
+            className='flex flex-col gap-3'
+            onSubmit={handleImageUpload}>
+            <Input
+                type='file'
+                accept="image/png, image/jpeg"
+                name='profilePicture'
+                id='profilePicture'
+                onChange={(e) => {
+                    console.log(e.target.files[0])
+                    setImage(e.target.files[0])
+                }}
+                disabled={loading}
+            />
+            <Button type='submit' color='primary' className='w-full' disabled={loading}>
+                Upload
+            </Button>
+            {
+                loading && (
+                    <Progress
+                        badge='Uploading Profile Picture'
+                        badgeIcon={<FiUploadCloud />}
+                        color='primary'
+                        completeMessage='Uploaded'
+                        infinite={true}
+                    />
+                )
+            }
+
+        </Form>
+    )
+}
+
+export default ProfilePictureUpload
--- src/components/pages/profile/index.js
+++ src/components/pages/profile/index.js
@@ -1,1 +1,6 @@
+// UserProfile
 export { default as UserProfile } from './UserProfile'
+// AddPhoneNumberForm
+export { default as PhoneNumberForm } from './PhoneNumberForm'
+// ProfilePictureUpload
+export { default as ProfilePictureUpload } from './ProfilePictureUpload'
