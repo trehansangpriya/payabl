023c513b6f17bf6df9ff5c2edb3ab440715f20fd
Trehan Sangpriya <trehansangpriya@gmail.com>
2022-07-27 15:22:17 +0530

    Updated Seperator: style-height

--- src/components/utility/Seperator/index.js
+++ src/components/utility/Seperator/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
-const Separator = ({ text, className = '' }) => {
+const Separator = ({ text, className = '', h }) => {
     return (
-        <div className={"flex w-full items-center justify-between " + className}>
+        <div className={"flex w-full items-center justify-between " + className} style={{
+            height: h
+        }}>
             <div className="w-full bg-layout-200 h-px"></div>
             {text && <span className="px-2">{text}</span>}
             <div className="w-full bg-layout-200 h-px"></div>
