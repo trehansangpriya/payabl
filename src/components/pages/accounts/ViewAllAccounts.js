94bec0d84b7c16e6c3f8287279afc56cc7747bfa
Trehan Sangpriya <trehansangpriya@gmail.com>
2022-07-29 14:20:37 +0530

    Added Component: pages/accounts/ViewAllAccounts

---
+++ src/components/pages/accounts/ViewAllAccounts.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+import { db } from '@/Firebase/index'
+import { collection, onSnapshot } from 'firebase/firestore'
+import useAuth from '@/Contexts/useAuth'
+import AccountCard, { AccountSkeleton } from './AccountCard'
+import { Container, Loading } from '@/Components/utility'
+
+const ViewAllAccounts = () => {
+    // Local Loading
+    const [loading, setLoading] = useState(false)
+    // Auth Context
+    const { user } = useAuth()
+    const [accounts, setAccounts] = useState([])
+    useEffect(() => {
+        setLoading(true)
+        onSnapshot(collection(db, 'users', user.uid, 'accounts'), (snapshot) => {
+            setAccounts(snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })))
+            setTimeout(() => {
+                setLoading(false)
+            }, 1000)
+        })
+    }, [user.uid])
+    return (
+        <Container
+            className='flex flex-col gap-1'
+        >
+            {loading ? <>
+                <AccountSkeleton />
+            </>
+                : (
+                    accounts.map(account => <AccountCard key={account.id} account={account} />)
+                )}
+        </Container>
+    )
+}
+
+export default ViewAllAccounts
--- src/components/pages/accounts/index.js
+++ src/components/pages/accounts/index.js
@@ -1,1 +1,3 @@
 export { default as AccountForm } from './AccountForm'
+export { default as ViewAllAccounts } from './ViewAllAccounts'
+export { default as AccountCard, AccountSkeleton } from './AccountCard'
