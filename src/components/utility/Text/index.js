ed1f6760ba75fe5bbbce2921e845c6a069d4308a
Trehan Sangpriya <trehansangpriya@gmail.com>
2022-07-29 14:15:14 +0530

    Updated Text: used switch case instead of createElement to help override classes when required

--- src/components/utility/Text/index.js
+++ src/components/utility/Text/index.js
@@ -16,27 +16,116 @@ const Text = ({
         'p': 'text-base font-medium',
         'span': 'text-base font-medium',
         'small': 'text-sm font-medium',
-        'strong': 'font-bold',
-        'em': 'font-italic',
-        'i': 'font-italic',
-        'b': 'font-bold',
-        'u': 'text-underline',
         'code': 'font-mono',
     }
-    const [textStyle, setTextStyle] = useState(textStyles[tag])
-    const [textClass, setTextClass] = useState(className)
-    const [textClassName, setTextClassName] = useState('')
-    useEffect(() => {
-        setTextStyle(textStyles[tag])
-        setTextClass(className)
-        setTextClassName(textStyle + ' ' + textClass)
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [tag, className])
+    // const [textStyle, setTextStyle] = useState(textStyles[tag])
+    // useEffect(() => {
+    //     setTextStyle(textStyles[tag])
+    //     // eslint-disable-next-line react-hooks/exhaustive-deps
+    // }, [tag])
+    switch (tag) {
+        case 'h1':
+            return (
+                <h1 className={[
+                    'font-bold text-4xl lg:text-5xl',
+                    className
+                ].join(' ')} {...props} >
+                    {children}
+                </h1>
+            )
+        case 'h2':
+            return (
+                <h2 className={[
+                    'font-bold text-3xl lg:text-4xl',
+                    className
+                ].join(' ')} {...props} >
+                    {children}
+                </h2>
+            )
+        case 'h3':
+            return (
+                <h3 className={[
+                    'font-bold text-2xl lg:text-3xl',
+                    className
+                ].join(' ')} {...props} >
+                    {children}
+                </h3>
+            )
+        case 'h4':
+            return (
+                <h4 className={[
+                    textStyles[tag],
+                    className
+                ].join(' ')} {...props} >
+                    {children}
+                </h4>
+            )
+        case 'h5':
+            return (
+                <h5 className={[
+                    textStyles[tag],
+                    className
+                ].join(' ')} {...props} >
+                    {children}
+                </h5>
+            )
+        case 'h6':
+            return (
+                <h6 className={[
+                    textStyles[tag],
+                    className
+                ].join(' ')} {...props} >
+                    {children}
+                </h6>
+            )
+        case 'p':
+            return (
+                <p className={[
+                    textStyles[tag],
+                    className
+                ].join(' ')} {...props} >
+                    {children}
+                </p>
+            )
+        case 'span':
+            return (
+                <span className=
+                    {[
+                        textStyles[tag],
+                        className
+                    ].join(' ')} {...props} >
+                    {children}
+                </span>
+            )
+        case 'small':
+            return (
+                <small className
+                    ={[
+                        textStyles[tag],
+                        className
+                    ].join(' ')} {...props}>
+                    {children}
+                </small>
+            )
+        case 'code':
+            return (
+                <code className=
+                    {[
+                        textStyles[tag],
+                        className
+                    ].join(' ')} {...props}>
+                    {children}
+                </code>
+            )
+    }
     return (
-        React.createElement(tag, {
-            className: textClassName,
-            ...props,
-        }, children)
+        <p className={[
+            'text-base font-medium',
+            className
+        ].join(' ')} {...props} >
+            {children}
+        </p>
+
     )
 }
 
