c7cc343f280b668f1cac510f619da9c3b429a4f1
Trehan Sangpriya <trehansangpriya@gmail.com>
2022-08-17 10:16:16 +0530

    Updated Card Component: UI styles

--- src/components/utility/Card/index.js
+++ src/components/utility/Card/index.js
@@ -4,14 +4,19 @@ const Card = ({
     children,
     pointer,
     className,
+    shadow,
+    rounded,
+    hover,
     ...props
 }) => {
 
     return (
         <div id='card' className={[
-            'shadow rounded p-3 m-1 flex gap-1 transition-all duration-300 ease-in-out bg-layout-100',
-            'hover:shadow-none',
+            'p-3 m-1 flex gap-1 transition-all duration-300 ease-in-out bg-layout-100',
             pointer && 'cursor-pointer',
+            shadow && 'shadow',
+            rounded && 'rounded',
+            hover && 'hover:brightness-95',
             className
         ].join(' ')} {...props} >
             {children}
