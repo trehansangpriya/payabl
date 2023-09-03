3669c27fea4a5821abcb2623ce333fcb04a6c46c
Trehan Sangpriya <trehansangpriya@gmail.com>
2023-09-03 22:15:12 +0530

    group accounts by account type

--- src/components/pages/accounts/ViewAllAccounts.js
+++ src/components/pages/accounts/ViewAllAccounts.js
@@ -13,6 +13,14 @@ const ViewAllAccounts = () => {
     // Auth Context
     const { user } = useAuth()
     const [accounts, setAccounts] = useState([])
+
+    const accountTypes = [
+        'Bank',
+        'Physical Wallet',
+        'Digital Wallet',
+        'Credit Card',
+    ]
+
     useEffect(() => {
         setLoading(true)
         onSnapshot(collection(db, 'users', user.uid, 'accounts'), (snapshot) => {
@@ -29,10 +37,36 @@ const ViewAllAccounts = () => {
             {loading ? <>
                 <AccountSkeleton />
             </>
-                : (
-                    <>
-                        <AccountsSummary accounts={accounts} />
-                        {accounts.length !== 0 ? accounts.map((account, index) => (
+                :
+                accounts.length === 0 ? <NotFound message='No accounts found' />
+                    : (
+                        <>
+                            <AccountsSummary accounts={accounts} />
+                            {/* Group by account type */}
+                            {accountTypes.map((accountType, index) => (
+                                <div key={index} className='mt-1'>
+                                    {/* if no accounts of this type, don't show the heading */}
+                                    {accounts.filter(account => account.accountType === accountType).length !== 0 &&
+                                        (
+                                            <>
+                                                <h5 className='font-semibold text-2xl py-2'>
+                                                    {accountType}s
+                                                </h5>
+                                            </>
+                                        )
+                                    }
+                                    {accounts.filter(account => account.accountType === accountType).map((account, index) => (
+                                        <div key={account.id}>
+                                            <AccountCard account={account} />
+                                            {
+                                                index !== accounts.length - 1 && <Seperator className='mt-1' />
+                                            }
+                                        </div>
+                                    ))}
+                                </div>
+                            )
+                            )}
+                            {/* {accounts.length !== 0 ? accounts.map((account, index) => (
                             <div key={account.id}>
                                 <AccountCard account={account} />
                                 {
@@ -40,9 +74,9 @@ const ViewAllAccounts = () => {
                                 }
                             </div>
                         ))
-                            : <NotFound message='No accounts found' />}
-                    </>
-                )}
+                            : <NotFound message='No accounts found' />} */}
+                        </>
+                    )}
         </Container>
     )
 }
